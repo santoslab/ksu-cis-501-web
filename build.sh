febb3cdef55060b3c803f0a666bc3e961b3a88eb
Robby <robby@k-state.edu>
2014-08-06 10:19:48 -0500

    Added build scripts.

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd src/main/rst
+touch source/notes.rst
+make html
+cd ../../..
---
+++ view.sh
@@ -0,0 +1,5 @@
+cd src/main/rst
+touch source/notes.rst
+make html
+cd ../../..
+open src/main/rst/build/html/index.html
