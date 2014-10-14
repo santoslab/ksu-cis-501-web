dd26fdbb69e2393e0de914eb9e030a7416daf026
Robby, Ph.D <robby@santoslab.org>
2014-10-14 13:33:58 -0500

    Added simpler race condition example.

---
+++ src/main/vs/MultiThreaded/RaceCondition/RaceCondition/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RaceCondition
+{
+    class Token
+    {
+        public int x = 0;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Token t = new Token();
+            new Thread(() => Run(t, 1)).Start();
+            new Thread(() => Run(t, 2)).Start();
+            Console.WriteLine("t.x = " + t.x);
+            Console.ReadLine();
+        }
+
+        static void Run(Token t, int id)
+        {
+            int temp = t.x + 1;
+            t.x = temp;
+        }
+    }
+}
