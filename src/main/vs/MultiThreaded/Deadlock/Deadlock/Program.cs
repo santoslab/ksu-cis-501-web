8896954316c184b940984ed6faece18087cba2a6
Robby, Ph.D <robby@santoslab.org>
2014-10-16 09:30:10 -0500

    Added deadlock example.

---
+++ src/main/vs/MultiThreaded/Deadlock/Deadlock/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Deadlock
+{
+    class Fork
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fork fork1 = new Fork();
+            Fork fork2 = new Fork();
+            new Thread(() => Run("Red", fork1, fork2)).Start();
+            new Thread(() => Run("Blue", fork2, fork1)).Start();
+            Console.ReadLine();
+        }
+
+        static void Run(String id, Fork left, Fork right)
+        {
+            Thread.Sleep(new Random().Next(0, 11));
+            Console.WriteLine(id + " takes left fork");
+            lock (left)
+            {
+                Thread.Sleep(new Random().Next(0, 11));
+                Console.WriteLine(id + " takes right fork");
+                lock (right)
+                {
+                    Thread.Sleep(new Random().Next(0, 11));
+                    Console.WriteLine(id + " thinks/eats");
+                }
+                Console.WriteLine(id + " puts down right fork");
+                Thread.Sleep(new Random().Next(0, 11));
+            }
+            Console.WriteLine(id + " puts down left fork");
+        }
+    }
+}
