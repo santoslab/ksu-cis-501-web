e22efc1738c260185cf89572dae488bc722978e2
Robby <robby@k-state.edu>
2015-02-11 14:15:26 -0600

    Updated to use GitHub Pages.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,15 @@
-cd src/main/rst
+rm -fR target
+mkdir target
+cd target
+git clone git@github.com:ksu-cis-501/gh-pages.git
+rm -fR gh-pages/*
+cd ../src/main/rst
 make clean
-touch source/notes.rst
+touch source/index.rst
 make html
-scp -r build/html/* cislinux.cis.ksu.edu:~santos/web/softwarearch.santoslab.org/
-cd ../../..
+cp -R build/html/* ../../../target/gh-pages/
+cd ../../../target/gh-pages
+echo "softwarearch.santoslab.org" > CNAME
+git add --all
+git commit -m 'Updated.'
+git push
